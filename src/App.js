da7974300d0a228c55548ab730538b4e7b213b9b
Kaustubh <kaustubhkulkarni035@gmail.comgit config --global user.name  Kaustubhgit config --global user.email kaustubhkulkarni035@gmail.com>
2023-06-16 13:26:16 +0530

    api integrate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import "./App.css";
+const Api_key = "592b59438cfbe0b73ba92b61ba34e755";
 
 function App() {
+  const inputRef = useRef(null);
+
+  const fetchWeather = async () => {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${inputRef.current.value}&units=metric&appid=${Api_key}`;
+    console.log(inputRef.current.value)
+    fetch(URL).then(res=> res.json())
+    .then(data=>{
+      console.log(data, "----data--")
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" y ref={inputRef} />
+      <button  onClick={fetchWeather}>click me </button>
+    </>
   );
 }
 
